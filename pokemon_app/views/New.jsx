6f5202106528f0ac32e3ef016c703a20c9f36b82
robertsamboy <106219442+robertsamboy@users.noreply.github.com>
2022-08-11 00:09:28 -0400

    making form, finishing it up soon as i wake up

--- pokemon_app/views/New.jsx
+++ pokemon_app/views/New.jsx
@@ -1,7 +1,17 @@
 const React = require('react');
 
 
-class New extends React.Component{}
+class New extends React.Component{
+    render() {
+        return(
+            <div>
+                <h1>Create your own Pokémon!</h1>
+                <form action = ''
+            </div>
+            
+        )
+    }
+}
 
 
 
